bc9f25c6a5f473f3144d2a629ecee2a127873aae
Chris Busch <cbusch@pivotal.io>
2015-12-06 16:27:15 -0600

    Fix payment-eureka... to payments-eureka...

--- delete-apps.sh
+++ delete-apps.sh
@@ -4,7 +4,7 @@ cf delete payments -f -r
 cf delete payment-service -f -r
 cf delete payment-history-service -f -r
 cf delete consumer-messaging-service -f -r
-cf delete payment-eureka-server -f -r
+cf delete payments-eureka-server -f -r
 
 # only in dev and test
 cf delete phpmyadmin-consumer -f -r
